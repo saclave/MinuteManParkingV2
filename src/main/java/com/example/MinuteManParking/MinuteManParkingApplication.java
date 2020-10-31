c60c16c80f526dbe15cd67713f41d1ef66a55d19
VEA MHAY JOY SACLAO (SHP-IRIS-ISD-OOCLL/MNL) <SACLAVE@oocl.com>
2020-10-31 12:45:37 +0800

    first commit

---
+++ src/main/java/com/example/MinuteManParking/MinuteManParkingApplication.java
@@ -0,0 +1,13 @@
+package com.example.MinuteManParking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinuteManParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinuteManParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MinuteManParking/MinuteManParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MinuteManParking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinuteManParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
