8808371447f20939f59697fd1de87182cd423d06
bacaljo <bacaljo@oocl.com>
2020-11-01 20:40:16 +0800

    [FrontEnd][John-Charlie] feature : Add logo in header

--- Front End/src/components/MPHeader.js
+++ Front End/src/components/MPHeader.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 
 import { Layout, Menu } from 'antd';
 
+import logo from '../images/minuteman-V2.png';
+
 const { Header } = Layout;
 
 class MPHeader extends Component {
     render() {
         return (
             <Header className="header">
-                <div className="logo" />
+                <div className="logo">
+                    <img width={100} src={logo} />
+                </div>
                 <Menu theme="dark" mode="horizontal">
                 </Menu>
             </Header>
