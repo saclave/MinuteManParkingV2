1a164b319c9ed93fec3dd92c6ebfab25115727ec
RAYMOND ALFRED ADANZA (IM-MIS-ISD-OOCLL/MNL) <ADANZRA@OOCL.COM>
2020-11-01 17:49:42 +0800

    [FrontEnd][Raymond] refactor : Remove unused codes

--- Front End/src/components/CreatePage.js
+++ Front End/src/components/CreatePage.js
@@ -1,31 +1,24 @@
 import React, { Component } from 'react';
-import { Form, Input, InputNumber, Button,DatePicker, Select } from 'antd';
+import { Form, Input, Button,DatePicker, Select } from 'antd';
      
 class createPage extends Component {
     render() {
         const layout = {
             labelCol: { span: 8 },
             wrapperCol: { span: 9 },
-            
           };
-
           const config = {
-            rules: [{ type: 'object', required: true, message: 'Please select time!' }],
+            rules: [{ type: 'object', required: true, message: 'Please select birthday!' }],
           };
-
           const validateMessages = {
             required: '${label} is required!',
             types: {
               email: '${label} is not validate email!',
-              number: '${label} is not a validate number!',
-            },
-            number: {
-              range: '${label} must be between ${min} and ${max}',
             },
           };
-
             const onFinish = values => {
               console.log(values);
+              this.props.addUser(values);
             };
             const { Option } = Select;  
               
