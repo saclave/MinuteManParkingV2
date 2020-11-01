78784ed1b23fa7142f50d7d7c63bf56b341e0832
RAYMOND ALFRED ADANZA (IM-MIS-ISD-OOCLL/MNL) <ADANZRA@OOCL.COM>
2020-11-01 23:31:58 +0800

    fix merge

--- Front End/src/actions.js
+++ Front End/src/actions.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-=======
-export const addUser = (parking) => {
-    return {type: "ADD_ACCOUNT", payload: parking}
-}
-
->>>>>>> 6aff67982ef90c6884907d5ca79e6bc7293924e1
 export const getAccount = accounts => {
     return { type: "GET_ACCOUNT", payload: accounts };
 }
