047e054addf9c9ac3ae014bbb107485daecc653a
FOZLAUR <fozla@oocl.com>
2020-11-03 19:10:40 +0800

    [MinuteMan-BE][Laurence] feature/refactor: Just added ParkingLotIntegrationTest and removed Dirties in UserIntegrationTest

--- src/test/java/com/example/MinuteManParking/integration/UserIntegrationTest.java
+++ src/test/java/com/example/MinuteManParking/integration/UserIntegrationTest.java
@@ -18,7 +18,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class UserIntegrationTest {
     public static final String USER_URI = "/users";
     @Autowired
