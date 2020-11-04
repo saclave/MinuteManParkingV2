b16fb2d1482bad5fe3bb454b48a965feb1531b70
VEA MHAY JOY SACLAO (SHP-IRIS-ISD-OOCLL/MNL) <SACLAVE@oocl.com>
2020-11-04 20:47:08 +0800

    [BE - Ticket String][Vea] fix - ticket string
    (cherry picked from commit 4eebc0d5d9d8c10bfe9cca520a1c394da86557ba)

--- src/main/java/com/example/MinuteManParking/service/TicketService.java
+++ src/main/java/com/example/MinuteManParking/service/TicketService.java
@@ -1,20 +1,32 @@
 package com.example.MinuteManParking.service;
 
 import com.example.MinuteManParking.exceptions.TicketNotFound;
+import com.example.MinuteManParking.model.ParkingLot;
+import com.example.MinuteManParking.model.ParkingSlot;
 import com.example.MinuteManParking.model.Ticket;
+import com.example.MinuteManParking.repository.ParkingLotRepository;
+import com.example.MinuteManParking.repository.ParkingSlotRepository;
 import com.example.MinuteManParking.repository.TicketRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
-import static com.example.MinuteManParking.exceptions.ExceptionConstants.*;
+import static com.example.MinuteManParking.exceptions.ExceptionConstants.TICKET_NOT_FOUND;
 
 @Service
 public class TicketService {
     private TicketRepository ticketRepository;
+    private ParkingSlotRepository parkingSlotRepository;
+    private ParkingLotRepository parkingLotRepository;
 
-    public TicketService(TicketRepository ticketRepository) {
+    public TicketService(TicketRepository ticketRepository, ParkingSlotRepository parkingSlotRepository,
+                         ParkingLotRepository parkingLotRepository) {
         this.ticketRepository = ticketRepository;
+        this.parkingSlotRepository = parkingSlotRepository;
+        this.parkingLotRepository = parkingLotRepository;
     }
 
     public List<Ticket> getAll() {
@@ -27,7 +39,22 @@ public class TicketService {
     }
 
     public Ticket create(Ticket ticket) {
+        ParkingSlot parkingSlot = parkingSlotRepository.findById(ticket.getParkingSlotId())
+                .orElse(null);
+        ParkingLot parkingLot = parkingLotRepository.findById(Objects.requireNonNull(parkingSlot).getParkingLotId())
+                .orElse(null);
+
+        ticket.setName(generateTicketString(Objects.requireNonNull(parkingLot).getName()) + "_"
+                + parkingSlot.getId() + "-" + ticket.getId());
+
         return ticketRepository.save(ticket);
+    }
+
+    private String generateTicketString(String str) {
+        return Arrays.stream(str.split(" "))
+                .map(s -> s.substring(0, 1))
+                .map(String::toUpperCase)
+                .collect(Collectors.joining());
     }
 
     public void delete(Integer ticketId) {
@@ -37,7 +64,6 @@ public class TicketService {
 
     public Ticket update(Integer ticketId, Ticket ticket) {
         Ticket retrievedTicket = retrieve(ticketId);
-        retrievedTicket.setName(ticket.getName());
         retrievedTicket.setTimeIn(ticket.getTimeIn());
         retrievedTicket.setTimeOut(ticket.getTimeOut());
         retrievedTicket.setAmount(ticket.getAmount());
