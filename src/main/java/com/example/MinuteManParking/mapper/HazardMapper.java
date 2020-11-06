54a873984112241d6a098ff03b5d23470fe8feb4
bacaljo <bacaljo@oocl.com>
2020-11-06 09:41:32 +0800

    [BE][John] fix - Add Mapping annotation to ignore unmapped fields in mappers

--- src/main/java/com/example/MinuteManParking/mapper/HazardMapper.java
+++ src/main/java/com/example/MinuteManParking/mapper/HazardMapper.java
@@ -4,12 +4,14 @@ import com.example.MinuteManParking.dto.HazardRequest;
 import com.example.MinuteManParking.dto.HazardResponse;
 import com.example.MinuteManParking.model.Hazard;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface HazardMapper {
     HazardMapper HAZARD_MAPPER = Mappers.getMapper(HazardMapper.class);
 
+    @Mapping(target = "id", ignore = true)
     Hazard toEntity(HazardRequest hazardRequest);
 
     HazardResponse toResponse(Hazard hazard);
--- src/main/java/com/example/MinuteManParking/mapper/TicketMapper.java
+++ src/main/java/com/example/MinuteManParking/mapper/TicketMapper.java
@@ -4,13 +4,16 @@ import com.example.MinuteManParking.dto.TicketRequest;
 import com.example.MinuteManParking.dto.TicketResponse;
 import com.example.MinuteManParking.model.Ticket;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface TicketMapper {
     TicketMapper TICKET_MAPPER = Mappers.getMapper(TicketMapper.class);
 
+    @Mapping(target = "id", ignore = true)
     Ticket toEntity(TicketRequest ticketRequest);
 
+    @Mapping(target = "parkingLotName", ignore = true)
     TicketResponse toResponse(Ticket ticket);
 }
